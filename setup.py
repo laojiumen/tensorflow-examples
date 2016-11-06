1f3f90d03c9aa3960d9848bff87f4938a58abab8
zq <zq.lu@foxmail.com>
2016-11-06 17:30:09 +0800

    mnist basic softmax

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='tensorflow-examples',
+    version='',
+    packages=['scripts'],
+    url='',
+    license='',
+    author='zq',
+    author_email='',
+    description=''
+)
